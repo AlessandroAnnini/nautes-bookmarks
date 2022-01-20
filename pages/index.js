99c9629edd73cc5d5e240c3b699da4bb8b61bb85
Alessandro Annini <alessandro.annini@gmail.com>
2022-01-20 20:35:17 +0100

    add date to home post data

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import ReactTooltip from 'react-tooltip';
+import * as dayjs from 'dayjs';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faListUl } from '@fortawesome/free-solid-svg-icons';
 import { Select, ButtonRotate } from '@/components';
@@ -11,6 +13,8 @@ import { tags } from '@/utils/tags';
 const websiteName = process.env.NEXT_PUBLIC_WEBSITE_NAME;
 
 const myTags = [{ value: '', label: 'all' }, ...tags];
+
+dayjs.extend(localizedFormat);
 
 export default function Home({ user, posts }) {
   const [displayedPosts, setDisplayedPosts] = useState([]);
@@ -67,7 +71,9 @@ export default function Home({ user, posts }) {
             <a className="block border-b border-gray-300	mt-8 pb-4">
               <h2 className="text-xl font-semibold">{post.title}</h2>
               <p className="text-gray-500 mt-2">
-                Tag: {post.tag} - Author: {post.author_name}
+                {`tag: ${post.tag} - author: ${
+                  post.author_name
+                } - date: ${dayjs().format('MMM D, YYYY')}`}
               </p>
             </a>
           </Link>
